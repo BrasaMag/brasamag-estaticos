88d9a5a1bbc30538695e9b7a23f6b789e6860b30
Ana Luiza Sampaio <sampaioaanaluiza@gmail.com>
2022-07-12 21:50:57 -0300

    Ajustar menu e animação

--- js/script.js
+++ js/script.js
@@ -1,16 +1,4 @@
 const nav = document.querySelector('nav');
-const botaoHamburguer = document.querySelector('.botao-hamburguer');
 
-function setStyle(element, prop, value) {
-    element.style[prop] = value;
-}
-
-botaoHamburguer.addEventListener('click', () => {
-    const navState = nav.style.display;
-
-    if (navState === 'none' || navState === '') {
-        setStyle(nav, 'display', 'flex');
-    } else {
-        setStyle(nav, 'display', 'none');
-    }
-})
+document.querySelector('.botao-hamburguer')
+    .addEventListener('click', () => nav.classList.toggle('active'))
