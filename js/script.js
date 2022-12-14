a7831a99f6981c8c718b5ff4998b0d709c49e0d3
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-12-14 17:46:56 -0300

    Ajustar form de pesquisa

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
-const nav = document.querySelector('nav');
+/** Navigation menu */
 
-document.querySelector('.botao-hamburguer')
-    .addEventListener('click', () => nav.classList.toggle('active'))
+const nav = document.querySelector("nav");
+const botaoMenu = document.querySelector(".botao-hamburguer");
+
+botaoMenu.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+/** Search form */
+
+const botaoPesquisa = document.querySelector("label");
+const inputPesquisa = document.querySelector("input");
+const fecharPesquisa = document.querySelector("form button");
+
+const toggleElements = () => {
+  inputPesquisa.classList.toggle("active");
+  fecharPesquisa.classList.toggle("active");
+};
+
+botaoPesquisa.onclick = () => toggleElements();
+fecharPesquisa.onclick = () => toggleElements();
+
+inputPesquisa.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") toggleElements();
+});
